4e414348958d4489801a154ca0d0c0af4808d084
prathana <prarthanakkmalu2000@gmail.com>
2024-08-28 09:48:15 +0530

    initial commit

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Products(models.Model):
+
+    name = models.CharField(unique=True,max_length=100)
+    price = models.PositiveIntegerField()
+    descripton = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    image = models.ImageField(null=True)
+
+    def __str__(self):
+       return self.name
+
+
+   
+
